d13550584cbe6c53bd031bdd28374cd575a04f8e
wangwei <275281462@qq.com>
2020-04-10 10:45:35 +0800

    新建项目

---
+++ app/src/main/java/com/remember/jetpackstudy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.remember.jetpackstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
