62991adca6da1445592e9a56b6b995b84d86503a
wangwei <275281462@qq.com>
2020-04-13 15:29:06 +0800

    网络请求

---
+++ libnetwork/src/main/java/com/remember/libnetwork/JsonCallback.java
@@ -0,0 +1,15 @@
+package com.remember.libnetwork;
+
+public abstract class JsonCallback<T> {
+    public void onSuccess(ApiResponse<T> response) {
+
+    }
+
+    public void onError(ApiResponse<T> response) {
+
+    }
+
+    public void onCacheSuccess(ApiResponse<T> response) {
+
+    }
+}
